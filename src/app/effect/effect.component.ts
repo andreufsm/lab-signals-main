d83be33bac1ab469708fef87158a7a8b16a925ca
andre-dias <andre-dias@procergs.rs.gov.br>
2025-03-24 14:49:45 -0300

    refatoração, camada de serviços

--- src/app/effect/effect.component.ts
+++ src/app/effect/effect.component.ts
@@ -1,12 +1,5 @@
-import { Component, effect, signal } from '@angular/core';
-
-interface Elemento {
-  nome: string;
-  simbolo: string;
-  numeroMassa: number;
-  pontoFusao: number;
-  pontoEbulicao: number;
-}
+import { Component } from '@angular/core';
+import { ElementoService, Elemento } from './../elemento.service';
 
 @Component({
   selector: 'app-effect',
@@ -14,84 +7,13 @@ interface Elemento {
   styleUrls: ['./effect.component.css'],
 })
 export class EffectComponent {
-  elementoSelecionado = signal<Elemento | null>(null);
-  temperatura = signal<number>(25);
-  estadoFisico = signal<string>('');
-
-  constructor() {
-    effect(
-      () => {
-        const elemento = this.elementoSelecionado();
-        const temp = this.temperatura();
-        if (elemento) {
-          let estadoFisico = '';
-          if (temp < elemento.pontoFusao) {
-            estadoFisico = 'Sólido';
-          } else if (
-            temp >= elemento.pontoFusao &&
-            temp < elemento.pontoEbulicao
-          ) {
-            estadoFisico = 'Líquido';
-          } else {
-            estadoFisico = 'Gasoso';
-          }
-          this.estadoFisico.set(estadoFisico);
-        }
-      },
-      { allowSignalWrites: true }
-    );
-  }
+  constructor(public elementoService: ElementoService) {}
 
   selecionarElemento(elemento: Elemento) {
-    this.elementoSelecionado.set(elemento);
+    this.elementoService.selecionarElemento(elemento);
   }
 
   ajustarTemperatura(novaTemperatura: number) {
-    this.temperatura.set(novaTemperatura);
+    this.elementoService.ajustarTemperatura(novaTemperatura);
   }
-
-  elementos: Elemento[] = [
-    {
-      nome: 'Hidrogênio',
-      simbolo: 'H',
-      numeroMassa: 1,
-      pontoFusao: -259,
-      pontoEbulicao: -253,
-    },
-    {
-      nome: 'Carbono',
-      simbolo: 'C',
-      numeroMassa: 12,
-      pontoFusao: 3550,
-      pontoEbulicao: 4027,
-    },
-    {
-      nome: 'Nitrogênio',
-      simbolo: 'N',
-      numeroMassa: 14,
-      pontoFusao: -210,
-      pontoEbulicao: -196,
-    },
-    {
-      nome: 'Oxigênio',
-      simbolo: 'O',
-      numeroMassa: 16,
-      pontoFusao: -218,
-      pontoEbulicao: -183,
-    },
-    {
-      nome: 'Sódio',
-      simbolo: 'Na',
-      numeroMassa: 23,
-      pontoFusao: 98,
-      pontoEbulicao: 883,
-    },
-    {
-      nome: 'Cloro',
-      simbolo: 'Cl',
-      numeroMassa: 35,
-      pontoFusao: -101,
-      pontoEbulicao: -34,
-    },
-  ];
 }
